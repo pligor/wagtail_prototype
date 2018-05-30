8111ae8ea837467cb524400510945257255fbd28
George <georgios.pligoropoulos@thekeysupport.com>
2018-05-30 17:17:46 +0100

    we made a new template tag for using a single advert for the blog page which currently has an one-to-one relationship with advert snippet

--- wagtailapp/templatetags/demo_tags.py
+++ wagtailapp/templatetags/demo_tags.py
@@ -1,15 +1,23 @@
-#http://docs.wagtail.io/en/v2.0/topics/snippets.html
+# http://docs.wagtail.io/en/v2.0/topics/snippets.html
 
 from django import template
 from wagtailapp.models_module.advert_snippet import Advert
 
-register = template.Library() #initiate register decorator
+register = template.Library()  # initiate register decorator
 
-@register.inclusion_tag('wagtailapp/simple_advert.html', #so this is considering the app
-                        takes_context=True #USEFUL
+
+@register.inclusion_tag('wagtailapp/simple_adverts.html',  # so this is considering the app / name of html
+                        takes_context=True  # USEFUL
                         )
 def adverts(context):
     return {
         'adverts': Advert.objects.all(),
         'request': context['request']
+    }
+
+
+@register.inclusion_tag('wagtailapp/simple_advert.html')
+def simple_advert(advert):
+    return {
+        'advert': advert,
     }
