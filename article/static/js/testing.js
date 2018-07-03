29eb97959f2999befca955c20851898c1f7fea8f
George <georgios.pligoropoulos@thekeysupport.com>
2018-07-03 14:32:24 +0100

    integrated vue.js with typescript and created the main component but not really integrated with django yet, only wrote some independent typescript file

---
+++ article/static/js/testing.js
@@ -0,0 +1,74 @@
+/// <reference path="node_modules/vue/types/vue.d.ts"/>
+import Vue from "vue";
+var vv = new Vue({
+    el: '#starting',
+    delimiters: ['${', '}'],
+    data: {
+        articles: [],
+        loading: false,
+        cur_article: {},
+        message: null,
+        new_article: {
+            'article_heading': null,
+            'article_body': null,
+        }
+    },
+    mounted: function () {
+        //things you want to happen at the beginning
+        this.getArticles();
+    },
+    methods: {
+        getArticles: function () {
+            this.loading = true;
+            this.$http.get('/article-api/article').then(function (response) {
+                this.articles = response.data;
+                this.loading = false;
+            }).catch(function (err) {
+                this.loading = false;
+                console.log(err);
+            });
+        },
+        getArticle: function (id) {
+            this.loading = true;
+            this.$http.get('/article-api/article/' + id + '/').then(function (response) {
+                this.cur_article = response.data;
+                this.loading = false;
+            }).catch(function (err) {
+                this.loading = false;
+                console.log(err);
+            });
+        },
+        addArticle: function () {
+            this.loading = true;
+            this.$http.post('/article-api/article/', this.newArticle).then(function (response) {
+                this.loading = false;
+                this.getArticles();
+            }).catch(function (err) {
+                this.loading = false;
+                console.log(err);
+            });
+        },
+        updateArticle: function () {
+            this.loading = true;
+            this.$http.put('/article-api/article/' + this.cur_article.article_id + '/', this.cur_article).then(function (response) {
+                this.loading = false;
+                this.cur_article = response.data;
+                this.getArticles();
+            }).catch(function (err) {
+                this.loading = false;
+                console.log(err);
+            });
+        },
+        deleteArticle: function (id) {
+            this.loading = true;
+            this.$http.delete('/article-api/article/' + id + '/').then(function (response) {
+                this.loading = false;
+                this.getArticles();
+            }).catch(function (err) {
+                this.loading = false;
+                console.log(err);
+            });
+        }
+    }
+});
+//# sourceMappingURL=testing.js.map
---
+++ article/static/js/testing.ts
@@ -0,0 +1,79 @@
+/// <reference path="node_modules/vue/types/vue.d.ts"/>
+import Vue from "vue";
+
+let vv = new Vue({
+    el: '#starting', //the id in the body we need it to run
+    delimiters: ['${', '}'], // delimiters are the tags which we apply around the vue js variables to display data in html file
+    data: {
+        articles: [],
+        loading: false,
+        cur_article: {},
+        message: null,
+        new_article: {
+            'article_heading': null,
+            'article_body': null,
+        }
+    },
+    mounted: function () { //runs before the mounting of the vue.js instance
+        //things you want to happen at the beginning
+        this.getArticles();
+    },
+    methods: {
+        getArticles: function () {
+            this.loading = true;
+            this.$http.get('/article-api/article').then(function (response: any) {
+                this.articles = response.data;
+                this.loading = false;
+            }).catch(function (err: any) {
+                this.loading = false;
+                console.log(err)
+            })
+        },
+
+        getArticle: function(id) {
+            this.loading = true;
+            this.$http.get('/article-api/article/' + id + '/').then(function (response: any) {
+                this.cur_article = response.data
+                this.loading = false;
+            }).catch(function (err: any) {
+                this.loading = false;
+                console.log(err)
+            })
+        },
+
+        addArticle: function() {
+            this.loading = true;
+            this.$http.post('/article-api/article/', this.newArticle).then(function(response: any) {
+                this.loading = false;
+                this.getArticles();
+            }).catch(function (err: any) {
+                this.loading = false;
+                console.log(err)
+            })
+        },
+
+        updateArticle: function() {
+            this.loading = true;
+            this.$http.put(
+                '/article-api/article/' + this.cur_article.article_id + '/', this.cur_article).then(function(response: any) {
+                this.loading = false;
+                this.cur_article = response.data;
+                this.getArticles();
+            }).catch(function (err: any) {
+                this.loading = false;
+                console.log(err)
+            })
+        },
+
+        deleteArticle: function(id) {
+            this.loading = true;
+            this.$http.delete('/article-api/article/' + id + '/').then(function(response: any) {
+                this.loading = false;
+                this.getArticles();
+            }).catch(function (err: any) {
+                this.loading = false;
+                console.log(err)
+            })
+        }
+    }
+});
