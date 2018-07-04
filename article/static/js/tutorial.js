7f49fb7169cf37c3e8230055bae688206f1a3008
George <georgios.pligoropoulos@thekeysupport.com>
2018-07-04 17:10:51 +0100

    we have created various components and dynamic components

--- article/static/js/tutorial.js
+++ article/static/js/tutorial.js
@@ -1,10 +1,51 @@
 "use strict";
+/// <reference path="../../../node_modules/vue/types/vue.d.ts"/>
+Vue.component('static-item', {
+    template: '<span>Company <b>is</b> the best</span>'
+});
+Vue.component('parametric', {
+    props: ['param1', 'param2'],
+    template: '<li>{{param1}}: {{param2}}</li>'
+});
 var app = new Vue({
     el: '#app',
     //this is necessary to avoid mixing up with Django template language
     delimiters: ['${', '}'],
     data: {
         message: 'Good to go',
+        mytitle: 'Nothing here really',
+        seen: true,
+        todos: [
+            'first todo',
+            'second todo',
+            'third thing',
+            'fourth todo',
+            'ktl.'
+        ],
     },
+    methods: {
+        add_curses: function (count) {
+            this.message = this.message + (" @^$%^%$$%^&@$".repeat(count));
+        },
+        get_numbered_todos: function () {
+            return this.todos.map(function (value, ind, arr) {
+                return {
+                    key: ind,
+                    val: value
+                };
+            });
+            /*let num_todos: object[] = [];
+            let count = 0;
+            for(let todo in this.todos) {
+                count += 1;
+                num_todos.push({
+                    key: count,
+                    val: this.todos[]
+                })
+            }
+
+            return num_todos*/
+        }
+    }
 });
 //# sourceMappingURL=tutorial.js.map
--- article/static/js/tutorial.ts
+++ article/static/js/tutorial.ts
@@ -1,3 +1,16 @@
+/// <reference path="../../../node_modules/vue/types/vue.d.ts"/>
+// import {Vue} from "vue/types/vue";
+declare var Vue;
+
+Vue.component('static-item', {
+    template: '<span>Company <b>is</b> the best</span>'
+});
+
+Vue.component('parametric', {
+    props: ['param1', 'param2'],
+    template: '<li>{{param1}}: {{param2}}</li>'
+});
+
 let app = new Vue({
     el: '#app', //the id in the body we need it to run
 
@@ -6,5 +19,39 @@ let app = new Vue({
 
     data: {
         message: 'Good to go',
+        mytitle: 'Nothing here really',
+        seen: true,
+        todos: [
+            'first todo',
+            'second todo',
+            'third thing',
+            'fourth todo',
+            'ktl.'
+        ],
     },
+
+    methods: {
+        add_curses: function (count: number) {
+            this.message = this.message + (" @^$%^%$$%^&@$".repeat(count))
+        },
+        get_numbered_todos: function() {
+            return this.todos.map((value, ind, arr) => {
+                return {
+                    key: ind,
+                    val: value
+                }
+            });
+            /*let num_todos: object[] = [];
+            let count = 0;
+            for(let todo in this.todos) {
+                count += 1;
+                num_todos.push({
+                    key: count,
+                    val: this.todos[]
+                })
+            }
+
+            return num_todos*/
+        }
+    }
 });
